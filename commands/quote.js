ae750b0355359b54450ba5264db0c164ea4c3ce2
nXn00 <nxn.forlive@gmail.com>
2020-06-17 21:19:27 +0300

    Update quote.js

--- commands/quote.js
+++ commands/quote.js
@@ -9,12 +9,21 @@ module.exports = {
     roleRestricted: true,
     execute(message, args) {
         const { Users, Admins, Roles, Events } = require('../dbObjects');
-        const lastDate = await Events.max('event_date', { where: { userId: killer.id }});
+        const lastDate = await Events.max('event_date');
+        if (!lastDate) {message.reply('в базе нет событий!')};
         const now = Date.now();
-        const totalTks = await Events.count({ where: { userId: killer.id }});
-        let reply;
-        if (lastDate) {
-            const msPast = now - lastDate;
-        const quote = message.args.join(/ +/);
+        const msPast = now - lastDate;
+        if (msPast <= 120000) {
+            const quote = args.join(/ +/);
+            await Events.update({
+                quote: quote
+            }, {
+                where: {
+                    event_date: lastDate
+                }
+            });
+        } else {
+            message.reply('припозднился ты.')
+        }   
     }
 }
